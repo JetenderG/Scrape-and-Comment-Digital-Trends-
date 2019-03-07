ca667817ba8dae6753e389623327857743d3842c
Jetender <g.jetender@gmail.com>
2019-03-06 23:55:42 -0500

    egsgfsg

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 //var db = require("./models/index");
 var PORT = process.env.PORT || 3000;
 
-var mongdb_uri = process.env.mongdb_uri;
+var mongdb_uri = process.env.mongdb_uri || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(mongdb_uri)
 
