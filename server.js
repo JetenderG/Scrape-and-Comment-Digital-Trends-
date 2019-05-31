78ed43be9f86acda1103e863b5a52e5cb28fa8de
Jetender <g.jetender@gmail.com>
2019-05-31 00:44:46 -0400

    Sefs

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 //var db = require("./models/index");
 var PORT = process.env.PORT || 3000;
 
-var mongdb_uri = process.env.mongdb_uri || "mongodb://heroku_042v2vt7:tqpvdohndm4ban6plsvc5b6h7i@ds133136.mlab.com:33136/heroku_042v2vt7";
+var mongdb_uri = process.env.mongdb_uri || "mongodb://localhost/mongoHeadlines" || "mongodb://heroku_042v2vt7:tqpvdohndm4ban6plsvc5b6h7i@ds133136.mlab.com:33136/heroku_042v2vt7";
 
 mongoose.connect(mongdb_uri)
 
